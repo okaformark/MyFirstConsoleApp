5c9604f4c4e6b43becf25cd7fd384c89d63968b1
Mark Okafor <okaformark@gmail.com>
2019-08-23 21:29:56 -0500

    exercises solved

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter strings seperated with a comma");
+            var stringInput = Console.ReadLine();
+            var variables = stringInput.Split(",");
+            var result = 1;
+            for (int i = 0; i < variables.Length; i++)
+            {
+                var intAnswer = int.Parse(variables[i]);
+                result = intAnswer * result;
+            }
+            Console.WriteLine($"The reult is {result}");
         }
     }
 }
